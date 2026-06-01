fc16a7e9ff1e4c516431e984d216bfbdb0fd9584
pramod-khapre <pramodkhapre3@gmail.com>
2026-06-01 19:08:42 +0530

    Create calculator.py

---
+++ calculator.py
@@ -0,0 +1,40 @@
+print(" Welcome To Python Calculater")
+
+print("""
+
+1. Addition 
+2. Subtraction
+3. Multiplication
+4. Division
+5. Exit
+
+""")
+
+choice = input("Choice the right number")
+
+if choice == "1":
+    num1 = int(input("Enter your grater number"))
+    num2 = int(input("Enter Your smaler number"))
+    if choice == "1":
+        print("Your Addition is" , num1 + num2)
+            
+elif choice == "2":
+    num1 = int(input("Enter your grater number"))
+    num2 = int(input("Enter Your smaler number"))
+    if choice == "2":
+        print(num1 - num2)
+
+elif choice == "3":
+    num1 = int(input("Enter your grater number"))
+    num2 = int(input("Enter Your smaler number"))
+    if choice == "3":
+        print(num1 * num2)
+
+elif choice == "4":
+    num1 = int(input("Enter your grater number"))
+    num2 = int(input("Enter Your smaler number"))
+    if choice == "4":
+        print(num1 / num2)
+
+else:
+    print("Bhai Sahi Number Daal")
